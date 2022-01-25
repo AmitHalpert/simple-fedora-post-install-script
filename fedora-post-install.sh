7543e84b709620b87d7685fe756d508340f7dd58
Amit Ethan Halpert <97952926+AmitHalpert@users.noreply.github.com>
2022-01-25 16:55:10 +0200

    Update fedora-post-install.sh
    Now it should work

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -4,16 +4,19 @@ echo "Intial dnf and firmware updates"
 sudo dnf update -y \
 
 #add some flags to the dnf conf file to speed it up
-echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf \
-echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf \
-echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf \
-cat /etc/dnf/dnf.conf \
+echo "Speeding Up DNF"
+echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
 
 
 
-echo "Adding additional rpmfusion free & nonfree repos"
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y \
-sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y \
+echo "Enabling RPM Fusion"
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+echo "Enabling Flatpak"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak update
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -29,23 +32,8 @@ sudo dnf update -y \
 sudo dnf install fedora-workstation-repositories -y \
 sudo dnf config-manager --set-enabled google-chrome -y \
 
-sudo dnf install \
--y \
-neofetch \
-git `#VCS done right` \
-google-chrome-stable \ 
-gnome-shell-extension-dash-to-dock `#dash for gnome` \
-gnome-tweaks `#Your central place to make gnome like you want` \
-htop `#Cli process monitor` \
-mpv `#The best media player (with simple gui)` \
-telegram-desktop `#Chatting, with newer openssl and qt base!` \
-virt-manager `#A gui to manage virtual machines` \
-yt-dlp `#Allows you to download and save youtube videos but also to open their links by dragging them into mpv!` \
-vim `#the better vim` \
-akmod-nvidia `#install nvidia driver` \
-xorg-x11-drv-nvidia-cuda \
-code \
-google-chrome-stable \
+echo "Installing Software"
+sudo dnf install -y gnome-extensions-app gnome-tweaks neofetch git google-chrome-stable gnome-shell-extension-dash-to-dock gnome-tweaks htop mpv telegram-desktop virt-manager yt-dlp vim akmod-nvidia xorg-x11-drv-nvidia-cuda code google-chrome-stable
 
 #Install flatpak Discord because RPMF Discord not working right now :(
 flatpak install flathub com.discordapp.Discord \
