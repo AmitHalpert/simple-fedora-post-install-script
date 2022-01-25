7f73345a9617f8864a3129521ef873140bae33a9
Amit Ethan Halpert <97952926+AmitHalpert@users.noreply.github.com>
2022-01-25 17:33:55 +0200

    Update fedora-post-install.sh
    Finishing Touches

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -20,18 +20,38 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf update -y
 
-echo "Installing flatpak (needed for certain shitware)"
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "Updating once again..."
 sudo dnf update -y
 
+###
+# Install base packages and applications
+###
 
 echo "Installing Software"
-sudo dnf install -y gnome-extensions-app gnome-tweaks neofetch git gnome-shell-extension-dash-to-dock gnome-tweaks htop mpv telegram-desktop virt-manager yt-dlp vim akmod-nvidia xorg-x11-drv-nvidia-cuda code
+sudo dnf install \
+-y \ 
+gnome-extensions-app \
+gnome-tweaks \
+neofetch \
+git \
+gnome-shell-extension-dash-to-dock \
+gnome-tweaks \
+htop \
+mpv \
+telegram-desktop \
+virt-manager \
+yt-dlp \
+vim akmod-nvidia \
+xorg-x11-drv-nvidia-cuda \
+code
+
+# Virtual Machines
+sudo systemctl enable --now libvirtd
 
 echo "Installing flatpak Discord"
 flatpak install flathub com.discordapp.Discord
+
 
 #Recovering maximize, minimize buttons
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
