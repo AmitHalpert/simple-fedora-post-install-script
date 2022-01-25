d0b085129df4ee1039fb14c47319d726c99e503d
Amit Ethan Halpert <97952926+AmitHalpert@users.noreply.github.com>
2022-01-25 16:40:16 +0200

    Update fedora-post-install.sh
    Fixed everything

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -3,7 +3,7 @@
 echo "Intial dnf and firmware updates"
 sudo dnf update -y \
 
-#I added some flags to the dnf conf file to speed it up
+#add some flags to the dnf conf file to speed it up
 echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf \
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf \
 echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf \
@@ -12,24 +12,25 @@ cat /etc/dnf/dnf.conf \
 
 
 echo "Adding additional rpmfusion free & nonfree repos"
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y \
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y \
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y \
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y \
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf update -y \
 
 echo "Installing flatpak (needed for certain shitware)"
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y \
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo \
 
-echo "Updating dnf repos once again..."
+echo "Updating once again..."
 sudo dnf update -y \
 
 #Enable the Google Chrome repo
 sudo dnf install fedora-workstation-repositories -y \
 sudo dnf config-manager --set-enabled google-chrome -y \
 
-sudo dnf install -y \
+sudo dnf install \
+-y \
 neofetch \
 git `#VCS done right` \
 google-chrome-stable \ 
