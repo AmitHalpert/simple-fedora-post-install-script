34d87ed739914fceb1f4c477b6a3ccde2b62f965
Amit Ethan Halpert <97952926+AmitHalpert@users.noreply.github.com>
2022-01-25 11:33:42 +0200

    Update fedora-post-install.sh
    Added Finishing Touches

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -15,16 +15,15 @@ sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfre
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update
+sudo dnf check-update -y
 
 echo "Installing flatpak (needed for certain shitware)"
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
 
 echo "Updating dnf repos once again..."
 sudo dnf upgrade --refresh -y
 
-echo "Installing jitt shit..."
-sudo dnf config-manager --set-enabled google-chrome
+sudo dnf config-manager --set-enabled google-chrome -y
 
 sudo dnf install -y \
 git `#VCS done right` \
@@ -39,9 +38,9 @@ yt-dlp `#Allows you to download and save youtube videos but also to open their l
 vim `#the better vim` \
 akmod-nvidia `#install nvidia driver` \
 xorg-x11-drv-nvidia-cuda \
-code
+code \
 
-flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.discordapp.Discord -y
 
 
 
