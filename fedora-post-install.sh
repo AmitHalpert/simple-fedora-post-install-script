d69e587cf5df0c19282c57e43e6da112d284ba98
Amit Ethan Halpert <97952926+AmitHalpert@users.noreply.github.com>
2022-01-25 18:08:49 +0200

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -18,8 +18,6 @@ flatpak update
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf update -y
-
 
 echo "Updating once again..."
 sudo dnf update -y
