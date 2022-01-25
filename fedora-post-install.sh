d8a1a213ed12c4c1e30594a47c7c06c060ca99b7
Amit Ethan Halpert <97952926+AmitHalpert@users.noreply.github.com>
2022-01-25 17:24:49 +0200

    Update fedora-post-install.sh
    Removed DeltaRPM

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -7,7 +7,6 @@ sudo dnf update -y
 echo "Speeding Up DNF"
 echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
-echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
 
 
 echo "Enabling RPM Fusion"
