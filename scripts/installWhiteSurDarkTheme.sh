f994489ef9ab051e54aac47c0caa2e94c8a14e25
Amit Ethan Halpert <97952926+AmitHalpert@users.noreply.github.com>
2023-04-25 15:10:41 +0300

    Update installWhiteSurDarkTheme.sh

--- scripts/installWhiteSurDarkTheme.sh
+++ scripts/installWhiteSurDarkTheme.sh
@@ -1,28 +1,10 @@
 #! /bin/bash
-# ./scripts/installWhiteSurDarkTheme.sh
-while true; do
-    read -rp "do you want to install all the packages above? [y/N]: " yn
-    
-    case $yn in
-    [Yy]*) 
-        echo "Installing WhiteSur-Dark theme"
-        
-        
-        TMPDIR=$(mktemp -d)
-        git clone https://github.com/vinceliuice/WhiteSur-gtk-theme $TMPDIR && $TMPDIR/install.sh
-        gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-Dark' 
-        gsettings set org.gnome.desktop.wm.preferences theme 'WhiteSur-Dark'
-        notify-send "Installed WhiteSur-Dark theme"
-        read -rp "Press any key to continue" _
-        
-        break
-    ;; 
 
-    *) 
-        echo "Aborted"
-        break
-    ;;
-    esac
-done
+echo "Installing WhiteSur-Dark theme"
 
+TMPDIR=$(mktemp -d)
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme $TMPDIR && $TMPDIR/install.sh
+gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-Dark' 
+gsettings set org.gnome.desktop.wm.preferences theme 'WhiteSur-Dark'
+notify-send "Installed WhiteSur-Dark theme"
 read -rp "Press any key to continue" _
